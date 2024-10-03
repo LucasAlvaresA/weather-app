0252fe1f19a633f4a4efa9683437b7567bac70a3
LucasAlvaresA <lucas.alvaress@hotmail.com>
2024-10-03 07:18:42 -0300

    🎉 create vite basic project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyle } from "./GlobalStyle";
+
+function App() {
+    return (
+        <>
+            <GlobalStyle />
+            <h1>hi</h1>
+        </>
+    );
+}
+
+export default App;
